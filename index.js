137fab25d2bf545207057c6f469e50aa38af2832
AnandDev006 <anand.dev.006@gmail.com>
2019-07-03 21:53:16 +0530

    pre pre dep

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const cheerio = require('cheerio');
 const nodemailer = require('nodemailer');
 const mongoose = require('mongoose');
 
+var http = require('http');
+
+//create a server object:
+http
+	.createServer(function(req, res) {
+		res.write('Hello World!'); //write a response to the client
+		res.end(); //end the response
+	})
+	.listen(8080); //the server object listens on port 8080
+
 const URLs = require('./URLSchema');
 
 const getPrice = async productURL => {
